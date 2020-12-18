609a9be5e49a5e0cb9c0961baa9764fef82d2b49
Laura Vieira <lauramvieira.aus@gmail.com>
2020-12-18 20:27:15 -0300

    pre heroku

--- src/server.js
+++ src/server.js
@@ -3,5 +3,5 @@ const port = process.env.PORT;
 const app = require('./app');
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port || 3000}`);
+  console.log(`Server is running on port ${port}`);
 });
